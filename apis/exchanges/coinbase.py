de2986420b3b48904df4e6c2303ab461f51ed453
Alasdair <alasdairjohnpurves@gmail.com>
2021-01-22 16:10:14 +0000

    Get raw transaction history from Coinbase

--- apis/exchanges/coinbase.py
+++ apis/exchanges/coinbase.py
@@ -1,34 +1,7 @@
 import os
-import time
 import requests
-from requests.auth import AuthBase
-import hmac
-import hashlib
 
-
-class CoinbaseAuth(AuthBase):
-    def __init__(self, api_key, api_secret):
-        self.api_key = api_key
-        self.api_secret = api_secret
-
-    def __call__(self, request):
-        timestamp = str(int(time.time()))
-        message = timestamp + request.method + request.path_url + (request.body or '')
-
-        if not isinstance(message, bytes):
-            message = message.encode()
-        if not isinstance(self.api_secret, bytes):
-            self.api_secret = self.api_secret.encode()
-
-        signature = hmac.new(self.api_secret, message, hashlib.sha256).hexdigest()
-
-        request.headers.update({
-            'CB-ACCESS-SIGN': signature,
-            'CB-ACCESS-TIMESTAMP': timestamp,
-            'CB-ACCESS-KEY': self.api_key,
-        })
-
-        return request
+from apis.authentication import CoinbaseAuth
 
 
 class Coinbase:
@@ -37,12 +10,104 @@ class Coinbase:
         self.api_secret = os.environ.get('COINBASE_API_SECRET')
         self.base_url = 'https://api.coinbase.com/v2/'
 
-    def temp(self):
+    def get_coinbase_transactions(self):
+        """
+        Method to execute all other methods in correct order to return all historical transactions from Coinbase.
+        """
+
+        # Get list of all wallets Coinbase allows
+        wallets = self.get_wallets()
+
+        # Loop through all wallets and ping its transactions endpoint to see which assets have transactions
+        wallets_with_transactions = []
+        for wallet in wallets:
+            if self.transactions_exist(wallet):
+                wallets_with_transactions.append(wallet)
+
+        # Loop through wallets we know have transactions and return a dict with all historical transactions
+        all_transactions = {}
+        for wallet in wallets_with_transactions:
+            all_transactions[wallet['asset']] = self.get_transactions(wallet['id'])
+
+        # Convert all transactions into pandas dataframes
+
+        return None
+
+    @staticmethod
+    def pagination(response):
+        if response['pagination']['next_uri']:
+            return response['pagination']['next_uri']
+
+    def get_wallets(self):
+        """
+        Function to return a list of dicts containing information on each asset wallet.
+        """
+
+        # Set up authentication and make call to the API
         auth = CoinbaseAuth(self.api_key, self.api_secret)
         r = requests.get(self.base_url+'accounts', auth=auth).json()
-        print(r)
+
+        wallets = []
+        for wallet in r['data']:
+            if wallet['type'] == 'wallet':
+                d = {
+                    'id': wallet['id'],
+                    'asset': wallet['currency']['code'],
+                    'current_balance': wallet['balance']['amount']
+                }
+                wallets.append(d)
+
+        return wallets
+
+    def transactions_exist(self, wallet):
+        """
+        Method to determine whether a give asset wallet has any transactions
+        """
+
+        # Set up authentication and make call to the API
+        auth = CoinbaseAuth(self.api_key, self.api_secret)
+        r = requests.get(self.base_url + f"accounts/{wallet['id']}/transactions", auth=auth).json()
+
+        # If there are transactions return True
+        if len(r['data']) > 0:
+            return True
+
+    def get_transactions(self, account_id):
+        """
+        Return all transactions for a given account id
+        """
+
+        # Initialise empty list to hold all individual transactions
+        transactions = []
+
+        # Set up authentication and make call to the API
+        auth = CoinbaseAuth(self.api_key, self.api_secret)
+        r = requests.get(self.base_url+f'accounts/{account_id}/transactions', auth=auth).json()
+
+        # Add first page of results to list
+        for transaction in r['data']:
+            transactions.append(transaction)
+
+        # Check whether we need to call multiple pages of transactions
+        pagination = Coinbase.pagination(r)
+
+        while pagination:
+            # Set up authentication and make call to the API
+            auth = CoinbaseAuth(self.api_key, self.api_secret)
+            r = requests.get(self.base_url+'/'.join(pagination.split('/')[2:]), auth=auth).json()
+
+            # Add new page of results to list
+            for transaction in r['data']:
+                transactions.append(transaction)
+
+            # Check for further pagination
+            pagination = Coinbase.pagination(r)
+
+        # Return full set of transactions
+        return transactions
 
 
 if __name__ == '__main__':
     x = Coinbase()
-    x.temp()
+    x.get_coinbase_transactions()
+    # x.get_transactions('f7b61c43-93c9-5b94-b4b0-719e09ad9888')
