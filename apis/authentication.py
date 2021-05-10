94cfa221a0694db033326f642db936afc77b0cb6
Alasdair <alasdairjohnpurves@gmail.com>
2021-05-10 11:32:27 +0100

    Add Binance auth and code to get all transactions

--- apis/authentication.py
+++ apis/authentication.py
@@ -3,6 +3,7 @@ from requests.auth import AuthBase
 import hmac
 import hashlib
 import base64
+from urllib.parse import urlencode
 
 
 class CoinbaseAuth(AuthBase):
@@ -40,15 +41,6 @@ class CoinbaseProAuth(AuthBase):
         timestamp = str(time.time())
         message = timestamp + request.method + request.path_url + (request.body or '')
 
-        # if not isinstance(message, bytes):
-        #     message = message.encode()
-        # if not isinstance(self.api_secret, bytes):
-        #     self.api_secret = self.api_secret.encode()
-        #
-        # hmac_key = base64.b64decode(self.api_secret)
-        # signature = hmac.new(hmac_key, message, hashlib.sha256)
-        # signature_b64 = signature.digest()#.encode('base64').rstrip('\n')
-
         message = message.encode('ascii')
         hmac_key = base64.b64decode(self.api_secret)
         signature = hmac.new(hmac_key, message, hashlib.sha256)
@@ -62,11 +54,22 @@ class CoinbaseProAuth(AuthBase):
         })
         return request
 
-        # request.headers.update({
-        #     'CB-ACCESS-SIGN': signature_b64,
-        #     'CB-ACCESS-TIMESTAMP': timestamp,
-        #     'CB-ACCESS-KEY': self.api_key,
-        #     'CB-ACCESS-PASSPHRASE': self.passphrase,
-        #     'Content-Type': 'application/json'
-        # })
-        # return request
+
+class BinanceAuth:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+
+    def get_request_headers(self):
+        headers = {
+            'X-MBX-APIKEY': self.api_key
+        }
+
+        return headers
+
+    def get_request_signature(self, params):
+        query_string = urlencode(params)
+        signature = hmac.new(self.api_secret.encode('utf-8'), query_string.encode('utf-8'),
+                             hashlib.sha256).hexdigest()
+
+        return signature
